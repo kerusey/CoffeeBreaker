bd5051183a15d98ea2456fa02cc63ef6090f6405
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-04 17:35:42 +0300

    Coffee class description

---
+++ Barista.h
@@ -0,0 +1,34 @@
+/*
+	barista lib
+
+	input parameters from app called somehow
+*/
+
+#define espresso 1
+#define kapuchino 2
+#define latte 3
+#define latte_makiato 4
+#define cafe_crema 5  // FIXMYNAME
+#define americano 6 
+
+/*  also custom stuff like tseh 95 does  */
+
+class Barista {
+	
+	size_t DrinkType;
+	bool milk;
+	bool chokolate;
+	bool canella;
+	size_t shugar; // 1-3 cubes of shugar 
+
+public:
+
+	Barista(size_t DrinkType = espresso,
+			bool milk = false,
+			bool chokolate = false,
+			bool canella = false,
+			size_t shugar = 0); // WRITEME
+
+	~Barista();
+	
+};
