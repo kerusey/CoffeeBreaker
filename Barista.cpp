f12ad0910a6bdc374af0a44d69b074cfd3ed66cb
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-05 22:04:10 +0300

    Add files via upload

---
+++ Barista.cpp
@@ -0,0 +1,12 @@
+#include "Barista.h"
+
+Barista::Barista(size_t _DrinkType,
+				 bool _milk,
+				 bool _chokolate,
+				 bool _canella,
+				 size_t _shugar) : DrinkType(_DrinkType), milk(_milk), chokolate(_chokolate), canella(_canella), sugar(_shugar) {}  
+
+Barista::~Barista() {}
+
+Barista::getBarista (Barista _cashed_one) {
+}
