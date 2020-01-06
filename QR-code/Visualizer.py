8a5f2c5749b99a43f3a3c6df0eec364a94bbfd15
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-06 19:20:42 +0300

    QRLink generator
    Works pretty fine

---
+++ QR-code/Visualizer.py
@@ -0,0 +1,15 @@
+import qrcode
+import string 
+import random
+
+def generateString (len = 25):
+	return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits ) for _ in range(len))
+
+Suffix = "#¯\\_(ツ)_/¯#" 
+AppLink = "https://coffeebreaker.com"; # SETME FIXME !
+UnicID = "1" # SETME FIXME
+UnicKey = generateString() # OK 
+QRLink = AppLink + Suffix + UnicID + Suffix + UnicKey + Suffix # SAVEME
+
+img = qrcode.make(QRLink)
+img.show()
