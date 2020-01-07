77879eba7b3b65fdc989852a3d78a98bf3fd5da8
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-07 15:09:59 +0300

    Add files via upload
    added auto-connection to the WiFi hotspot

--- Coffee Machine/install.sh
+++ Coffee Machine/install.sh
@@ -1,7 +1,12 @@
 sudo su -
 apt-get update
+apt-get upgrade
+raspi-config
+iwlist wlan0 scan
+nano /etc/wpa_supplicant/wpa_supplicant.conf # network = { ssid="SSIDNAME"  psk="SSIDPASSWORD" }
 apt-get install python3
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 pip install -U pip
 pip install qrcode
+pip install
