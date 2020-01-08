85b811ebea3f6df47cb3d890efd35f33ac16b263
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-09 01:55:39 +0300

    Generates order token and starts visual session

--- Coffee Machine/Validation/Visualizer.py
+++ Coffee Machine/Validation/Visualizer.py
@@ -1,6 +1,10 @@
 import qrcode
 import string 
 import random
+import time
+from pynput.keyboard import Key, Controller
+
+#import Tkinter 
 
 def generateString (len = 25):
 	return ''.join(random.SystemRandom().choice(string.ascii_letters + string.digits ) for _ in range(len))
@@ -10,11 +14,20 @@ AppLink_const = "https://coffeebreaker.com"; # SETME FIXME !
 UnicID_const = "1" # SETME FIXME
 
 def generateQRLink ():
-    UnicToken = generateString()
-    return AppLink_const +  Suffix_const + UnicID_const +  Suffix_const + UnicToken +  Suffix_const, UnicToken # QRLink func
+    UnicKey = generateString()
+    return AppLink_const +  Suffix_const + UnicID_const +  Suffix_const + UnicKey +  Suffix_const, UnicKey # QRLink func
 
-def visualNewSession(QRLink):   # !!! generates NEW token and starts NEW session !!!
+def visualNewSession():   # !!! generates NEW token and starts NEW session !!!
     QRLink, token = generateQRLink()
     current_image = qrcode.make(QRLink)
+    # Tk.attributes("-fullscreen", True)
     current_image.show()
     return token
+
+def closeWindow(): # OK
+    time.sleep(2)
+    keyboard = Controller()
+    keyboard.press(Key.alt)
+    keyboard.press(Key.f4)
+    keyboard.release(Key.alt)
+    keyboard.release(Key.f4)
