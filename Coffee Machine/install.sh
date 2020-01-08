94f53eee84edc3a9cfd896ce5de356e172d62837
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-08 18:43:31 +0300

    Update install.sh

--- Coffee Machine/install.sh
+++ Coffee Machine/install.sh
@@ -1,15 +1,24 @@
 sudo su -
 apt-get update
-apt-get upgrade
+apt-get -y upgrade
+apt-get -y install git
 raspi-config
 iwlist wlan0 scan
 nano /etc/wpa_supplicant/wpa_supplicant.conf # network = { ssid="SSIDNAME"  psk="SSIDPASSWORD" }
-apt-get install python3
+apt-get -y install python3
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 pip install -U pip
 pip install qrcode
-pip install
-apt-get install realvnc-vnc-server
+apt-get -y install realvnc-vnc-server
 vncserver :1 # 5901 / 5902
 apt-get -y install python3-rpi.gpio
+cd ~/Downloads
+apt-get install python3-tk
+apt-get install vsftpd
+nano /etc/vsftpd.conf # annonymous enble (NO) ; local enable ; write enable ; local umask ; chroot local user ; user sub token ; local root  
+cd home/<username> # SETME FIXME
+mkdir FTP
+mkdir FTP/files
+chmod a-w FTP
+service vsftpd restart
