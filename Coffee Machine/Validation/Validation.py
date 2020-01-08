63fee01d6818beceaad447996ca53bcaa7bdd7ab
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-09 01:54:51 +0300

    Compares server token and current (real) token

--- Coffee Machine/Validation/Validation.py
+++ Coffee Machine/Validation/Validation.py
@@ -1,14 +1,15 @@
-import qrcode
 import Visualizer
 import Server
 
-current_token = Visualizer.visualNewSession() # initialize QRCode and save current token  
+current_token = Visualizer.visualNewSession() #### initialize QRCode and save current token  
 
 def serverCheck(token = current_token):
-	if (Server.getToken() != token):
-		return Visualizer.visualNewSession()
+	if (Server.getToken() != token): 
+		Visualizer.closeWindow()
 		# also app have to drop session => need feedback # FIXME
+		return Visualizer.visualNewSession()
 	else: # if (Server.getToken() == token)
 		pass # app need another feedback to resume buyer session # FIXME
 		return token
 
+# token = serverCheck() # OK
