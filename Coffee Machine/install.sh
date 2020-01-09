b5bfc0576f5b92752e47be03987f5987387ace03
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-09 23:10:47 +0300

    Add files via upload

--- Coffee Machine/install.sh
+++ Coffee Machine/install.sh
@@ -2,6 +2,8 @@ sudo su -
 apt-get update
 apt-get -y upgrade
 apt-get -y install git
+git clone https://github.com/kerusey/CoffeeBreaker
+cp CoffeeBreaker/MachineSettings.json  /home/pi/Documents
 raspi-config
 iwlist wlan0 scan
 nano /etc/wpa_supplicant/wpa_supplicant.conf # network = { ssid="SSIDNAME"  psk="SSIDPASSWORD" }
@@ -10,17 +12,10 @@ curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 pip3 install -U pip
 pip3 install Pynput
+pip3 install --upgrade google-api-python-client google-auth-httplib2 google-auth-oauthlib
 pip3 install qrcode
 apt-get -y install realvnc-vnc-server
 vncserver :1 # 5901 / 5902
 apt-get -y install python3-rpi.gpio
-cd ~/Downloads
 apt-get install python3-tk
-apt-get install vsftpd
-nano /etc/vsftpd.conf # annonymous enble (NO) ; local enable ; write enable ; local umask ; chroot local user ; user sub token ; local root  
-cd home/pi 
-mkdir FTP
-mkdir FTP/validation
-mkdir FTP/order 
-chmod a-w FTP
-service vsftpd restart
+mkdir /home/pi/Server
