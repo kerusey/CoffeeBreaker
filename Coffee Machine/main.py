b85fa1a4f64eece865e20377ab8ca5fad9ddd8eb
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-24 15:32:31 +0300

    Add files via upload

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -1,1 +1,16 @@
-#TODO 6
+# TODO 6
+# Main file of CoffeeMachine
+
+import Visualizer
+import Barista
+
+def tokens():
+	while(cashe_token != token):
+		token = visualNewSession()
+		cahse_token = validteToken(Server.getToken())
+		Server.await()
+
+
+def main():
+	tokens()
+	
