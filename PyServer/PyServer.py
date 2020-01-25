ba2ea08593f6069dec800dcfdab036457d4a7cd4
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-25 15:47:12 +0300

    Update PyServer.py

--- PyServer/PyServer.py
+++ PyServer/PyServer.py
@@ -1,21 +1,47 @@
 from flask import Flask, request
-
+import json
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/name")
 def hello():
+	print("val")
 	return "Hello World!"
 
-@app.route('/postjson', methods = ['POST'])
-def postJsonHandler():
-	print (request.is_json)
+@app.route('/postOrder', methods = ['POST'])
+def postJsonOrder():
 	content = request.get_json()
-	print(content['device'])
-	print(content['value'])
-	print(content['timestamp'])
-	return 'JSON posted'
+	jjson = { "MachineID": str(content['MachineID']),
+			"type": str(content['type']),
+			"strenght": int(content['strenght']),
+			"volume": int(content['volume']),
+			"milk": bool(content['milk']),
+			"chokolate": bool(content['chokolate']),
+			"canella": bool(content['canella']),
+			"shugar": int(content['shugar'])
+		}
+	js = json.dumps(jjson, sort_keys=True, indent=4, separators=(',', ': '))
+	with open ('Order.json', 'w+') as f:
+		f.write(js) 
+	return "#¯\\_(ツ)_/¯#"   # OK
+
+@app.route('/postToken', methods = ['POST'])
+def postJsonToken():
+	content = request.get_json()
+	jjson = {'token': content['token']}
+	js = json.dumps(jjson, sort_keys=True, indent=4, separators=(',', ': '))
+	with open ('Token.json', 'w+') as f:
+		f.write(js) 
+	return "#¯\\_(ツ)_/¯#"   # OK
+
+@app.route('/getToken')
+def getJsonToken():
+	SITE_ROOT = os.path.realpath(os.path.dirname(__file__))
+    json_url = os.path.join(SITE_ROOT,'static', 'test.json')
+    token = open(json_url)
+    stored_json = token.readlines()
+	token.close()
+	return stored_json # FIXME
 
 if __name__ == '__main__':
 	app.run(host="172.16.0.37", port=8090)
-
