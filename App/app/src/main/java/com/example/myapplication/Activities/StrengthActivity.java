0332d0dfa8e39b2472b2c3de662ff98bc7c8bfeb
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-27 11:26:32 +0300

    Add files via upload

---
+++ App/app/src/main/java/com/example/myapplication/Activities/StrengthActivity.java
@@ -0,0 +1,53 @@
+package com.example.myapplication.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.Toast;
+
+import com.example.myapplication.MainActivity;
+import com.example.myapplication.R;
+
+public class StrengthActivity extends AppCompatActivity {
+    SeekBar seekBar;
+    Button btn_strong;
+    public static String STRENGTH_CHOICE;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_strength);
+
+        btn_strong = findViewById(R.id.btn_strong);
+        btn_strong.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(StrengthActivity.this, ResultActivity.class));
+
+            }
+        });
+
+        seekBar= findViewById(R.id.seekBar);
+        seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged (SeekBar seekBar,int progress,
+            boolean fromUser){
+                STRENGTH_CHOICE = String.valueOf(seekBar.getProgress());
+                Toast.makeText(getApplicationContext(), "Степень:" + progress, Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onStartTrackingTouch (SeekBar seekBar){
+                Toast.makeText(getApplicationContext(), "Регулируйте ползунком ", Toast.LENGTH_SHORT).show();
+            }
+            @Override
+            public void onStopTrackingTouch (SeekBar seekBar){
+                Toast.makeText(getApplicationContext(), "Подтвердите!", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+}
---
+++ App/app/src/main/java/com/example/myapplication/Activities/ValuemActivity.java
@@ -0,0 +1,43 @@
+package com.example.myapplication.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myapplication.R;
+
+public class ValuemActivity extends AppCompatActivity implements View.OnClickListener {
+Button btn_2, btn_4;
+public static String VALUEM_CHOICE;
+public static String PRICE;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_valuem);
+
+        btn_2 = findViewById(R.id.btn_2);
+        btn_2.setOnClickListener(ValuemActivity.this);
+
+        btn_4 = findViewById(R.id.btn_4);
+        btn_4.setOnClickListener(ValuemActivity.this);
+    }
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_2:
+                VALUEM_CHOICE=String.valueOf(0.2);
+                PRICE=String.valueOf(120);
+                startActivity(new Intent(ValuemActivity.this, StrengthActivity.class));
+                break;
+            case R.id.btn_4:
+                VALUEM_CHOICE=String.valueOf(0.4);
+                PRICE=String.valueOf(240);
+                startActivity(new Intent(ValuemActivity.this, StrengthActivity.class));
+                break;
+
+        }
+    }
+}
