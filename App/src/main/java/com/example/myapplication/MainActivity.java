b6fcae71f289b30ecf40146b4ad795e734679487
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-27 11:30:52 +0300

    Add files via upload

---
+++ App/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+
+public class MainActivity extends AppCompatActivity {
+    ImageButton btn_login;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        btn_login = findViewById(R.id.btn_login);
+        btn_login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(MainActivity.this, MenuActivity.class));
+
+            }
+        });
+    }
+}
