c5b310c674fc18074751383021728ad5df146a98
kerusey <38439184+kerusey@users.noreply.github.com>
2020-01-29 15:07:05 +0300

    Update main.py

--- Coffee Machine/main.py
+++ Coffee Machine/main.py
@@ -1,16 +1,8 @@
-# TODO 6
-# Main file of CoffeeMachine
 
-import Visualizer
-import Barista
-
-def tokens():
-	while(cashe_token != token):
-		token = visualNewSession()
-		cahse_token = validteToken(Server.getToken())
-		Server.await()
-
+from Validation import Visualizer
+from Barista import Order
 
 def main():
-	tokens()
+	current_token = Visualizer.visualNewSession()
+	validateToken()
 	
