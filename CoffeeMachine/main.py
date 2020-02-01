2e32f776d1c4d30c87b10b389b7ec045bb1c517b
kerusey <38439184+kerusey@users.noreply.github.com>
2020-02-02 00:06:05 +0300

    Update main.py

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -3,10 +3,26 @@ from Validation import Visualizer
 from Barista import Order
 import Request
 
+def validateToken(currentToken):
+	validationStatus = Visualizer.validateToken(currentToken)
+	if(validationStatus):
+		Request.postTokenStatus("OK")
+		return currentToken, True
+	else:
+		Request.postTokenStatus("FAILED")
+		Visualizer.stopSession()
+		currentToken = Visualizer.visualNewSession()
+		return currentToken, False
+		
+
 def main():
 	currentToken = Visualizer.visualNewSession()
-	validationStatus = Visualizer.validateToken(currentToken)
-	if(not validationStatus):
-            Visualizer.stopSession()
+	tokenStatus = False
+	while(not tokenStatus):
+		currentToken, tokenStatus = validateToken(currentToken)
+		
+		
+	
 
 main()
+
