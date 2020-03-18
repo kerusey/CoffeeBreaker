93e1379e39362c21d1e87ce26d15b33937eb6c7b
syori <enderdron3@gmail.com>
2020-03-19 00:24:36 +0200

    Added default project for re:creating

---
+++ NewApp/app/src/main/java/space/fstudio/lio/coffeebreaker/MainActivity.java
@@ -0,0 +1,14 @@
+package space.fstudio.lio.coffeebreaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
