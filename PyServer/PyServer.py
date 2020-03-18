59881c839a6e5e877cc1e0b36d5f4b1b178e8528
kerusey <38439184+kerusey@users.noreply.github.com>
2020-03-18 19:26:08 +0300

    Update PyServer.py

--- PyServer/PyServer.py
+++ PyServer/PyServer.py
@@ -24,48 +24,48 @@ def hello():
 
 @app.route('/postOrder/<id>', methods = ['POST'])
 def postJsonOrder(id):
-	path = "./Orders/"
+	path = "Orders/"
 	filename = "Order" + str(id)
 	content = request.get_json()
-	jjson = { "MachineID":str(content['MachineID']),
-			"type":str(content['type']),
-			"strenght":int(content['strenght']),
-			"volume":int(content['volume']),
-			"milk":bool(content['milk']),
-			"shugar":int(content['shugar'])
+	jjson = { "MachineID": int(content['MachineID']),
+			"type": str(content['type']),
+			"strenght": int(content['strenght']),
+			"volume": int(content['volume']),
+			"milk": bool(content['milk']),
+			"shugar": int(content['shugar'])
 		}
-	js = json.dumps(jjson, sort_keys=True, indent=4, separators=(',', ':'))
+	js = json.dumps(jjson, sort_keys=True, indent=4, separators=(',', ': '))
 	with open (path + filename + '.json', 'w+') as f:
 		f.write(js)
-	return "#¯\\_(ツ)_/¯#"   # OK
+	return "#"   # OK
 
 @app.route('/postToken/<id>', methods = ['POST'])
 def postJsonToken(id):
-	path = "./Tokens/"
+	path = "Tokens/"
 	filename = "Token" + str(id)
 
 	content = request.get_json()
 	jjson = {'token':content['token']}
-	js = json.dumps(jjson, sort_keys=True, indent=4, separators=(',', ':'))
+	js = json.dumps(jjson, sort_keys=True, indent=4, separators=(',', ': '))
 	with open (path + filename + '.json', 'w+') as f:
 		f.write(js)
-	return "#¯\\_(ツ)_/¯#"   # OK
+	return "#"   # OK
 
 @app.route('/postTokenStatus/<id>', methods = ['POST'])
 def postTokenStatus(id):
-	path = "./TokenStatuses/"
+	path = "TokenStatuses/"
 	filename = "TokenStatus" + str(id)
 
 	content = request.get_json()
 	jjson = {'status':content['status']}
-	js = json.dumps(jjson, sort_keys=True, indent=4, separators=(',', ':'))
+	js = json.dumps(jjson, sort_keys=True, indent=4, separators=(',', ': '))
 	with open (path + filename + '.json', 'w+') as f:
 		f.write(js)
-	return "#¯\\_(ツ)_/¯#"   # OK
+	return "#"   # OK
 
 @app.route('/postOrderStatus/<id>', methods = ['POST'])
 def postOrderStatus(id):
-	path = "./OrderStatuses/"
+	path = "OrderStatuses/"
 	filename = "OrderStatus" + str(id)
 
 	content = request.get_json()
@@ -73,11 +73,11 @@ def postOrderStatus(id):
 	js = json.dumps(jjson, sort_keys=True, indent=4, separators=(',', ': '))
 	with open (path + filename + '.json', 'w+') as f:
 		f.write(js)
-	return "#¯\\_(ツ)_/¯#"   # OK
+	return "#"   # OK
 
 @app.route('/getToken/<id>') #  OK
 def getJsonToken(id):
-	path = "./Tokens/"
+	path = "Tokens/"
 	filename = 'Token' + str(id) + '.json'
 	if(os.path.isfile(path + filename)):
 		return jsonify(getExistance(path + filename))
@@ -86,13 +86,13 @@ def getJsonToken(id):
 
 @app.route('/getOrder/<id>') #  OK
 def getJsonOrder(id):
-	path = "./Orders/"
+	path = "Orders/"
 	filename = 'Order' + str(id) + '.json'
 	return jsonify(getExistance(path + filename))
 
 @app.route('/getTokenStatus/<id>') #  OK
 def getTokenStatus(id):
-	path = "./TokenStatuses/"
+	path = "TokenStatuses/"
 	filename = 'TokenStatus' + str(id) + '.json'
 	jsonFile = getExistance(path + filename)
 	if (jsonFile == "0"):
@@ -102,7 +102,7 @@ def getTokenStatus(id):
 
 @app.route('/getOrderStatus/<id>') #  OK
 def getOrderStatus(id):
-	path = "./OrderStatuses/"
+	path = "OrderStatuses/"
 	filename = 'OrderStatus' + str(id) + '.json'
 	jsonFile = getExistance(path + filename)
 	if (jsonFile == "0"):
