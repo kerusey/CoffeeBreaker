fb99d76e576abeb27d92d24ed9e98710af800d24
kerusey <38439184+kerusey@users.noreply.github.com>
2020-03-20 22:59:13 +0300

    Add files via upload

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -1,12 +1,51 @@
-
 from Validation import Visualizer
 from Barista import Order
 import Request
+import time
 
-def main():
+def scanActivity():
 	currentToken = Visualizer.visualNewSession()
-	validationStatus = Visualizer.validateToken(currentToken)
-	if(not validationStatus):
-            Visualizer.stopSession()
+	currentToken = 0
+	Request.postTokenStatus("ReadyToScan")
 
+	timer = 1
+	while(Request.getTokenStatus() == "ReadyToScan"):
+		time.sleep(timer)
+		timer += 1
+		if (timer >= 90):
+			timer = 1      # OK
+	
+	validationStatus = Visualizer.validateToken(currentToken)
+	Request.postTokenStatus(validationStatus)
+	if(validationStatus == "FAILED"):
+		Visualizer.stopSession()
+		os.remove(ssesion.png) # FAILCHECK
+		time.sleep(10)
+		return True
+	
+	return False
+	
+def finalOrder():
+	Request.postOrderStatus("Waiting")
+	
+	timer = 1
+	while(Request.getOrderStatus() == "Waiting"):
+		time.sleep(timer)
+		timer += 1
+		if (timer >= 90):
+			timer = 1      # OK
+	
+	return Request.getOrder()
+	
+	
+def main():
+	flag = True
+	while flag:
+		flag = scanActivity()
+	jsonOrder = finalOrder()
+	
+	# Barista.make(jsonOrder) 
+	
+	
+	
 main()
