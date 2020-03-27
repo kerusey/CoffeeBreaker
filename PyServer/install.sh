6ed37dc1c12c2640cf2c771106641475448c5404
kerusey <38439184+kerusey@users.noreply.github.com>
2020-03-27 03:00:25 +0300

    Create install.sh

---
+++ PyServer/install.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+Operating_System=lsb_release -i | cut -f 2-
+if [ $Operating_System == "CentOS" ]
+then
+    sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm;
+    sudo yum update;
+    sudo yum install -y python36u python36u-libs python36u-devel python36u-pip;
+elif [ $Operating_System == "Ubuntu" ]
+then
+    sudo apt update;
+    sudo apt install python3;
+elif [ $Operating_System == "Ubuntu" || $Operating_System == "Debian" ]
+then
+    sudo apt update;
+    sudo apt install python3;
+elif [ $Operating_System == "Arch" || $Operating_System == "ManjaroLinux" ]
+then
+    sudo pacman -S python3;
+else
+    echo "Unsupported Operating System. Please install python3 by yourself";
+sudo python3 -m pip3 install --upgrade pip
+sudo pip3 install -r requirements.txt
