2c06a5589d0ef2c597b1221f89b7139344d062d4
kerusey <38439184+kerusey@users.noreply.github.com>
2020-03-29 01:17:53 +0300

    Update install.sh

--- CoffeeMachine/install.sh
+++ CoffeeMachine/install.sh
@@ -4,6 +4,13 @@ cp CoffeeBreaker/MachineSettings.json  /home/pi/Documents
 sudo apt-get -y install python3
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
+sudo apt-get install build-essential python-dev python-smbus git
+cd
+git clone https://github.com/adafruit/Adafruit_Python_MCP3008.git
+cd Adafruit_Python_MCP3008
+sudo python setup.py install
+cd ..
+rm -rf Adafruit_Python_MCP3008
 sudo pip3 -r install requrements.txt
 sudo apt-get -y install python3-rpi.gpio
 sudo apt-get install python3-tk
