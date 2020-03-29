af95bdb68d6ecac9bf8e7375ead943bfcda1e6ec
BlueBlood-dev <62560825+BlueBlood-dev@users.noreply.github.com>
2020-03-29 23:06:18 +0300

    Update BDConnection.java

--- DataBase/src/BDConnection.java
+++ DataBase/src/BDConnection.java
@@ -7,24 +7,24 @@ public class BDConnection {
 
     public BDConnection() {
         try{
-            String url = "jdbc:mysql://178.206.224.58:3306/coffeeBreaker?serverTimezone=Europe/Moscow";
-            String username = "coffeeBreaker";
-            String password = "coffeeBreaker";
+            String url = "your database URL"; // watchout it might be a timezone error (use ?Timezone: )
+            String username = "your username";
+            String password = "your username";
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
             try (Connection connection = DriverManager.getConnection(url, username, password)){
 
                 System.out.println("Connection to coffeeBreaker BD has been completed!");
                try {
-                   new JavaBDPrint(connection).print();
-                   new ValueInsert(connection).insert();
+                   new JavaBDPrint(connection).print();// printing Data Base values
+                   new ValueInsert(connection).insert(); // inserts values 
                }catch (Exception ignore) {
-                   System.out.println("proeb");
+                   System.out.println("proeb");// was used to catch printing troubles. 
 
                }
             }
         }
         catch(Exception ex){
-            System.out.println("Connection failed...");
+            System.out.println("Connection failed..."); // could occur if you didn t import Driver as a library. 
 
             System.out.println(ex);
         }
