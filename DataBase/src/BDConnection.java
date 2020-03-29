8b842a9e85409eba8f36c101dd7a28c8c8c76e6d
BlueBlood-dev <62560825+BlueBlood-dev@users.noreply.github.com>
2020-03-29 22:46:36 +0300

    Rebuilded

--- DataBase/src/BDConnection.java
+++ DataBase/src/BDConnection.java
@@ -1,22 +1,35 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 
+
+
 public class BDConnection {
 
     public BDConnection() {
         try{
-            String url = "your URL";
-            String username = "your  username";
-            String password = "your password";
+            String url = "jdbc:mysql://178.206.224.58:3306/coffeeBreaker?serverTimezone=Europe/Moscow";
+            String username = "coffeeBreaker";
+            String password = "coffeeBreaker";
             Class.forName("com.mysql.cj.jdbc.Driver").getDeclaredConstructor().newInstance();
-            try (Connection c = DriverManager.getConnection(url, username, password)){
-            System.out.println("Connection to coffeeBreaker BD has been completed!");
+            try (Connection connection = DriverManager.getConnection(url, username, password)){
+
+                System.out.println("Connection to coffeeBreaker BD has been completed!");
+               try {
+                   new JavaBDPrint(connection).print();
+                   new ValueInsert(connection).insert();
+               }catch (Exception ignore) {
+                   System.out.println("proeb");
+
+               }
             }
         }
-        catch(Exception ex) {
+        catch(Exception ex){
             System.out.println("Connection failed...");
+
             System.out.println(ex);
         }
+
     }
+
 
 }
---
+++ DataBase/src/ValueInsert.java
@@ -0,0 +1,16 @@
+import java.sql.Connection;
+
+public class ValueInsert {
+    Connection connection;
+
+    public ValueInsert(Connection connection) { this.connection = connection; }
+    public void insert ()
+    {
+
+
+
+    }
+
+
+
+}
