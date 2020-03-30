6b6dff289f486e4940ec7ddd2d26bff3aa077324
kerusey <38439184+kerusey@users.noreply.github.com>
2020-03-30 22:26:20 +0300

    PyServer installation update

--- PyServer/install.sh
+++ PyServer/install.sh
@@ -4,18 +4,20 @@ Operating_System=lsb_release -i | cut -f 2-
 if [ $Operating_System == "CentOS" ]
 then
     sudo yum install -y https://centos7.iuscommunity.org/ius-release.rpm;
+    sudo yum install figlet;
+    figlet -f slant Hello;
     sudo yum update;
     sudo yum install -y python36u python36u-libs python36u-devel python36u-pip;
-elif [ $Operating_System == "Ubuntu" ]
-then
-    sudo apt update;
-    sudo apt install python3;
 elif [ $Operating_System == "Ubuntu" || $Operating_System == "Debian" ]
 then
+    sudo apt-get install figlet;
+    figlet -f slant Hello;
     sudo apt update;
     sudo apt install python3;
 elif [ $Operating_System == "Arch" || $Operating_System == "ManjaroLinux" ]
 then
+    sudo pacman -S figlet;
+    figlet -f slant Hello;
     sudo pacman -S python3;
 else
     echo "Unsupported Operating System. Please install python3 by yourself";
