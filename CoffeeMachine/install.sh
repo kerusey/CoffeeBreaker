51a8bc33ddbac832fb8c6c9e50821e718ab81c6a
kerusey <danila.lihh@gmail.com>
2020-03-31 21:47:43 +0300

    install.sh both fixed

--- CoffeeMachine/install.sh
+++ CoffeeMachine/install.sh
@@ -1,26 +1,18 @@
-sudo apt-get install figlet
-figlet -f slant Hello
-sudo apt install sl
 sudo apt-get update
+sudo apt install sl
 sudo apt-get -y upgrade
 cp CoffeeBreaker/MachineSettings.json  /home/pi/Documents
 sudo apt-get -y install python3
 curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
 python3 get-pip.py
 rm get-pip.py
+sudo pip3 -r install requrements.txt
 sudo apt-get install build-essential python-dev python-smbus python-pip
 sudo pip3 install adafruit-mcp3008
 sudo python setup.py install
 cd ..
-sudo pip3 -r install requrements.txt
 sudo apt-get -y install python3-rpi.gpio
 sudo apt-get install python3-tk
-mkdir /home/pi/Server
-cd /home/pi/Server/
-mkdir Tokens
-mkdir TokenStatuses
-mkdir Orders
-mkdir OrderStatuses
 cd
 sudo apt-get install imagemagick
 curl https://processing.org/download/install-arm.sh | sudo sh
