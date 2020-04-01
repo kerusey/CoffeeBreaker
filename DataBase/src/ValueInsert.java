24a1893e72b74a42d73181e3ddcc84fbb929b550
BlueBlood-dev <62560825+BlueBlood-dev@users.noreply.github.com>
2020-04-01 20:34:53 +0300

    Update ValueInsert.java

--- DataBase/src/ValueInsert.java
+++ DataBase/src/ValueInsert.java
@@ -1,16 +1,20 @@
 import java.sql.Connection;
+import java.sql.*;
+
 
 public class ValueInsert {
     Connection connection;
+    Statement statement;
 
     public ValueInsert(Connection connection) { this.connection = connection; }
-    public void insert () // work in progres... 
-    {
-
-
+    public void insert () throws SQLException {
+      statement = connection.createStatement();
+      String sql = "INSERT INTO num " + " VALUES (100, 100, 100, 100, '00:00', '12.09.2003' )"; // values will be changed to variables when the deal with .json starts
+      statement.executeUpdate(sql);
+        System.out.println("Inserted records into the table...");
+        new JavaBDPrint(connection).print();
 
     }
-
 
 
 }
