c5cc76be36d96eda6044dc84ede7431506f69021
kerusey <danila.lihh@gmail.com>
2020-04-27 02:02:48 +0300

    updated test build and updated config

--- CoffeeMachine/install.py
+++ CoffeeMachine/install.py
@@ -11,32 +11,17 @@ def visualize():
 	while(True):
 		system("sl")
 
-
 prefix = ["sudo"]
 suffix = [">", "/dev/null"]
 
-def shellRun(name:str):
-	subprocess.call([*prefix, "chmod", "+x", name])
-	subprocess.call([*prefix, "./", name, "-qq", *suffix])
-	subprocess.call([*prefix, "rm", name])
+aptHaveToBePreinstalled = ["tightvncserver",
+						   "sl",
+						   "sshpass"
+						  ]
 
-
-'''
-def getItemInPrefix(prefix:list):
-	arr = ()
-	for item in prefix:
-		arr += (item,)
-	return arr
-
-print(type(getItemInPrefix(prefix)))
-prefix = getItemInPrefix(prefix)
-'''
-
-haveToBePreinstalled = ["tightvncserver",
-						"pure-ftpd",
-						"sl",
-						"sshpass"
-						]
+pipHaveToBePreinstalled = ["netifaces",
+						   "pathlib",
+						  ]
 
 aptPackages = [ "build-essential",
 				"python-dev",
@@ -46,48 +31,54 @@ aptPackages = [ "build-essential",
 				"python3-rpi.gpio"
 				]
 
-# services = ["ssh", "vncserver", "ftp"]
+def getLan(): # OK
+	interfaces = netifaces.interfaces()
+	for i in interfaces:
+		if i == 'lo':
+			continue
+		iface = netifaces.ifaddresses(i).get(netifaces.AF_INET)
+		if iface != None:
+			for j in iface:
+				return str(j['addr'])
 
-def clear():
+def shellRun(name:str): # OK
+	subprocess.call([*prefix, "chmod", "+x", name])
+	subprocess.call([*prefix, "./", name, "-qq", *suffix])
+	subprocess.call([*prefix, "rm", name])
+
+def clear(): # OK
 	subprocess.call(["clear"])
 
-def initSshServer():
+def initSshServer(): # OK
 	password = str(getpass())
-	subprocess.call(["sshpass", "-p", password, "ssh", "pi@"+str(socket.gethostbyname(socket.gethostname()))])
 	subprocess.call(["sudo", "systemctl", "enable", "ssh"])
+	subprocess.call(["sshpass", "-p", password, "ssh", "pi@"+getLan()])
 	subprocess.call(["sudo", "systemctl", "start", "ssh"])
 
-def initVncServer(): # TestME
-	with open("/root/.vnc/config.d/vncserver-x11", "w") as writable, open("config/vncserver-x11", "r") as readable:
-		writeable.write(readabe.read())
+def initVncServer(): # OK
+	with open("/root/.vnc/config.d/vncserver-x11", "w") as serverWriteable, open("config/vncserver-x11", "r") as serverReadable:
+		serverWriteable.write(serverReadable.read())
 
 	print("Enter VNC password:")
-	subprocess.call(["sudo", "vncpasswd -service"])
+	subprocess.call([*prefix, "vncserver"])
 
 	pathlib.Path("/home/pi/.config/autostart").mkdir(parents=True, exist_ok=True)
 	with open("/home/pi/.config/autostart/tightvnc.desktop", "w") as writeable, open("config/tightvnc.desktop", "r") as readabe: # adding to the autostart
 		writeable.write(readabe.read())
 
-def initFtpServer():
-	subprocess.call([*prefix, "groupadd", "ftpgroup"])
-	subprocess.call([*prefix, "useradd", "ftpuser", "-g", "ftpgroup", "-s", "/sbin/nologin", "-d", "/dev/null"])
-	subprocess.call([*prefix, "pure-pw", "useradd", "upload", "-u", "ftpuser", "-g" "ftpgroup", "-d", "/home/pi", "-m"])
-	subprocess.call([*prefix, "pure-pw", "mkdb"])
-	subprocess.call([*prefix, "service", "pure-ftpd", "restart"])
 
-def initWifiAutoconnection(): # TestME
-	with open("/etc/network/interfaces", "w") as writeable, open("config/interfaces", "r") as readable:
-		for row in readable:
-			writeable.write(readable.read())
-	'''
-		print("failed to initialize interfaces")
-		return None
-	'''
-
+def initWifiAutoconnection(): # TestVersion
 	print("Enter SSID: ", end='')
 	ssid = str(input())
 	print("Enter Key or Password: ", end='')
 	password = str(getpass())
+
+	with open("/etc/network/interfaces", "w") as writeable, open("config/interfaces", "r") as readable:
+		for row in readable:
+			writeable.write(readable.read())
+		writeable.write("wpa-ssid " + ssid)
+		writeable.write("wpa-psk " + password)
+
 	with open("/etc/wpa_supplicant/wpa_supplicant.conf", "w+") as writeable, open("config/wpa_supplicant.conf") as readabe:
 		for number, row in enumerate(readabe):
 			writeable.write(readabe.read())
@@ -95,26 +86,16 @@ def initWifiAutoconnection(): # TestME
 		writeable.write('psk="KEY"'.format(psk=password))
 		writeable.write("}")
 
-def preinstall():# TestME
-	try:
-		for item in haveToBePreinstalled:
-			subprocess.call([*prefix, "apt-get", "-qq", "install", item])
-	except:
-		time.sleep(2)
-		print("awaiting for dpkg")
-		preinstall()
-
-	print("Enter SSH password:")
-	initSshServer()
-
-	print("Enter FTP password:")
-	initFtpServer()
-
-	print("Enter VNC password:")
-	initVncServer()
+def preinstall(): # TestVersion
+	subprocess.call([*prefix, "apt-get", "-qq", "update"])
+	for item in aptHaveToBePreinstalled:
+		subprocess.call([*prefix, "apt-get", "-qq", "install", item])
+	for item in pipHaveToBePreinstalled:
+		subprocess.call([*prefix, "pip3", "-q", "install", item])
+		import item
 
 
-def shellRun(name:str):
+def shellRun(name:str): # OK
 	subprocess.call([*prefix, "chmod", "+x", name])
 	system("sudo ./" + name + " > logs.log 2> /dev/null")
 	subprocess.call([*prefix, "rm", name])
@@ -141,7 +122,6 @@ except:
 def back(threadName, frontThread=frontThread):
 	time.sleep(1)
 	subprocess.call([*prefix, "curl", "-s", "https://bootstrap.pypa.io/get-pip.py", "-o", "get-pip.py", *suffix]) # OK
-	subprocess.call([*prefix, "apt-get", "update", "-qq"]) # OK
 	subprocess.call([*prefix, "apt-get", "upgrade", "--force-yes", "-qq"]) # OK
 	subprocess.call([*prefix, "cp", "MachineSettings.json",  "/home/pi/Documents"])
 	subprocess.call([*prefix, "python3", "-s", "get-pip.py", "-qq"])
