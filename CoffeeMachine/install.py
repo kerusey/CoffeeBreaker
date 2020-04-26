252dda41b95f9824c8a8a084b0b91567f48f9ea1
kerusey <38439184+kerusey@users.noreply.github.com>
2020-04-26 21:03:35 +0300

    a little bit tested
    pointed out methods that would be tested in a time

--- CoffeeMachine/install.py
+++ CoffeeMachine/install.py
@@ -3,7 +3,7 @@
 import socket
 from getpass import getpass
 import subprocess
-from os import *
+from os import system
 import time
 import threading
 
@@ -52,32 +52,51 @@ def clear():
 	subprocess.call(["clear"])
 
 def initSshServer():
-	print("Enter SSH password:", end='')
 	password = str(getpass())
-	subprocess.call(["sshpass", "-p", password, "ssh", "pi@".join(socket.gethostbyname(socket.gethostname()))])
+	subprocess.call(["sshpass", "-p", password, "ssh", "pi@"+str(socket.gethostbyname(socket.gethostname()))])
 	subprocess.call(["sudo", "systemctl", "enable", "ssh"])
 	subprocess.call(["sudo", "systemctl", "start", "ssh"])
 
-def initVncServer():
-	with open("/root/.vnc/config.d/vncserver-x11", "r") as readabe, open("vncserver-x11", "w") as writeable:
+def initVncServer(): # TestME
+	with open("/root/.vnc/config.d/vncserver-x11", "w") as writable, open("config/vncserver-x11", "r") as readable:
 		writeable.write(readabe.read())
 
 	print("Enter VNC password:")
 	subprocess.call(["sudo", "vncpasswd -service"])
 
-	with open("/home/pi/.config/autostart/tightvnc.desktop", "w") as writeable, open("config/tightvnc.desktop", "r") as readabe: # adding to the autostart
 	pathlib.Path("/home/pi/.config/autostart").mkdir(parents=True, exist_ok=True)
+	with open("/home/pi/.config/autostart/tightvnc.desktop", "w") as writeable, open("config/tightvnc.desktop", "r") as readabe: # adding to the autostart
 		writeable.write(readabe.read())
 
+
 def initFtpServer():
-	print("Enter FTP password:")
 	subprocess.call([*prefix, "groupadd", "ftpgroup"])
 	subprocess.call([*prefix, "useradd", "ftpuser", "-g", "ftpgroup", "-s", "/sbin/nologin", "-d", "/dev/null"])
 	subprocess.call([*prefix, "pure-pw", "useradd", "upload", "-u", "ftpuser", "-g" "ftpgroup", "-d", "/home/pi", "-m"])
 	subprocess.call([*prefix, "pure-pw", "mkdb"])
 	subprocess.call([*prefix, "service", "pure-ftpd", "restart"])
 
-def preinstall():
+def initWifiAutoconnection(): # TestME
+	with open("/etc/network/interfaces", "w") as writeable, open("config/interfaces", "r") as readable:
+		for row in readable:
+			writeable.write(readable.read())
+	'''
+		print("failed to initialize interfaces")
+		return None
+	'''
+	
+	print("Enter SSID: ", end='')
+	ssid = str(input())
+	print("Enter Key or Password: ", end='')
+	password = str(getpass())
+	with open("/etc/wpa_supplicant/wpa_supplicant.conf", "w+") as writeable, open("config/wpa_supplicant.conf") as readabe:
+		for number, row in enumerate(readabe):
+			writeable.write(readabe.read())
+		writeable.write('ssid="SSID"'.format(SSID=ssid))
+		writeable.write('psk="KEY"'.format(psk=password))
+		writeable.write("}")
+
+def preinstall():# TestME
 	try:
 		for item in haveToBePreinstalled:
 			subprocess.call([*prefix, "apt-get", "-qq", "install", item])
@@ -85,29 +104,16 @@ def preinstall():
 		time.sleep(2)
 		print("awaiting for dpkg")
 		preinstall()
+
+	print("Enter SSH password:")
 	initSshServer()
+	
+	print("Enter FTP password:")
 	initFtpServer()
+	
+	print("Enter VNC password:")
 	initVncServer()
 
-def initWifiAutoconnection(login:str, password:str):
-	try:
-		with open("/etc/network/interfaces", "w+") as writeable, open("config/interfases") as readable:
-			for row in readable:
-				writeable.write(readable.read())
-	except:
-		print("failed to initialize interfaces")
-		return None
-	try:
-		with open("/etc/wpa_supplicant/wpa_supplicant.conf", "w+") as writeable, open("config/wpa_supplicant.conf") as readabe:
-			for number, row in enumerate(readabe):
-				writeable.write(readabe.read())
-			writeable.write('ssid="SSID"'.format(SSID=login))
-			writeable.write('psk="KEY"'.format(psk=password))
-			writeable.write("}")
-	except:
-		print("failed to initialize wpa_supplicant.conf")
-		return None
-	return 0
 
 def shellRun(name:str):
 	subprocess.call([*prefix, "chmod", "+x", name])
@@ -151,9 +157,6 @@ def back(threadName, frontThread=frontThread):
 
 	subprocess.call(["curl", "-s", "https://processing.org/download/install-arm.sh", "-o", "install-arm.sh"])
 	shellRun("install-arm.sh")
-
-
-	initWifiAutoconnection(ssid, password)
 
 	subprocess.call("rm", "nohup.out")
 
