91b832c6527f07a4306bbe09b9d67ec44513b3f5
pavel_em <pavelemelyanov03@mail.ru>
2020-04-30 01:49:57 +0300

    change variables

---
+++ NewApp/app/src/main/java/space/fstudio/lio/coffeebreaker/Objects/OrderStatusJsonObject.java
@@ -0,0 +1,12 @@
+package space.fstudio.lio.coffeebreaker.Objects;
+
+public class OrderStatusJsonObject {
+    private String status;
+
+    public OrderStatusJsonObject(String status) {
+        this.status = status;
+
+    }
+}
+
+
