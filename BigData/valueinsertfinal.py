dd9dfb990134b607b50656faf0fc4c6988ca8277
BlueBlood-dev <62560825+BlueBlood-dev@users.noreply.github.com>
2020-05-06 15:36:03 +0300

    Update valueinsertfinal.py

--- BigData/valueinsertfinal.py
+++ BigData/valueinsertfinal.py
@@ -12,30 +12,24 @@ def getCredits(path: str = os.path.dirname(os.path.abspath(__file__)) + "\databa
 	        databaseCredits.append(row[:-1])
 	return databaseCredits
 
-'''def randomTime():
+def randomTime():
 	hrs = random.randint(0, 24)
 	mins = random.randint(0, 60)
 	if(int(mins) < 10):
 		mins = "0" + str(mins)
 	if(int(hrs) < 10):
 		hrs = "0" + str(hrs)
-	return str(str(hrs) + ":" + str(mins))'''
+	sec = "00"
+	return str(str(hrs) + ":" + str(mins) + ":" + sec)
 
-def getValues(days):#simplify this method 
-	coffee = 8
-	id_ = random.randint(1,8)
-	sugar = random.randint(0,4)
-	water = random.randint(2,4) * 100
-	milk = random.randint(0,2) * 100
-	'''if (days < 10):
-		mdate = "2020.01.0" + str(days)
+def getValues(days):
+	if (days < 10):
+		mdate = "2020-01-0" + str(days)
 	else:
-		mdate = "2020.01." + str(days)'''
+		mdate = "2020-01-" + str(days)
+	mdate +=randomTime
 
-	return id_,  water, sugar, milk
-
-
-
+	return random.randint(1, 8),  random.randint(2, 4) * 100,  random.randint(0, 4), random.randint(0, 2) * 100, mdate
 
 dbCredit = getCredits()
 dataBaseConnection = connector.connect(host = dbCredit[0],
@@ -46,7 +40,6 @@ passwd = dbCredit[2],
 sqlcursor = dataBaseConnection.cursor()
 sqlcursor.execute("USE coffeeBreaker;")
 
-
 def clearDatabase():
 	sql = "truncate coffeeBreaker.CoffeeBreakerDataTable;"
 	sqlcursor.execute(sql)
@@ -55,11 +48,9 @@ def clearDatabase():
 def insertDatabase():
 	for j in range(days):
 		for i in range(orders):
-			sql = "INSERT INTO CoffeeBreakerDataTable ( coffeeID, water, sugar, milk) VALUES (%s, %s, %s, %s)"
+			sql = "INSERT INTO CoffeeBreakerDataTable ( coffeeID, water, sugar, milk, date) VALUES (%s, %s, %s, %s, %s)"
 			sqlcursor.execute(sql, getValues(j+1))
 			dataBaseConnection.commit()
-
-
 
 
 
