63e1a9109cff1a775071cae8841574d97bc3eaac
pavel_em <pavelemelyanov03@mail.ru>
2020-05-13 22:59:56 +0300

    updating of bd

--- PyServer/PyServer.py
+++ PyServer/PyServer.py
@@ -4,6 +4,7 @@ import netifaces
 import os
 import json
 import requests
+import threading
 
 def getLan(): # OK
     interfaces = netifaces.interfaces()
@@ -15,17 +16,17 @@ def getLan(): # OK
             for j in iface:
                 return str(j['addr'])
 
+host = getLan()
+port = 8090
+path = os.path.dirname(os.path.abspath(__file__))+"/"
+app = Flask(__name__)
+api = Api(app)
 coffeeMachineClusterPool = {}
-with open("CoffeeMachineClusterPool.json") as jsonFile:
+
+with open(path+"CoffeeMachineClusterPool.json") as jsonFile:
     data = json.load(jsonFile)
     for row in data:
         coffeeMachineClusterPool[row] = data[row]
-
-host = getLan()
-port = 8090
-path = os.path.dirname(os.path.abspath(__file__))
-app = Flask(__name__)
-api = Api(app)
 
 ingredients = ( "coffeeType", "milk", "strength", "sugar", "volume" )
 dataBaseHeader = ( "coffeeID", "water", "sugar", "milk" )
@@ -35,23 +36,21 @@ class OrderFromApp(Resource):
         parser = reqparse.RequestParser()
         parser.add_argument("coffeeType", type=str, location='json')
         for item in ingredients:
-            if (item is "coffeeType"):
+            if (item == "coffeeType"):
                 continue
             parser.add_argument(item, type=int, location='json')
         args = parser.parse_args()
-        requests.post('http://' + str(coffeeMachineClusterPool[id]) + ":" + str(port) + "/ToCluster_<id>", json=json.dumps(args))
+        requests.post('http://' + str(coffeeMachineClusterPool[id]) + ":" + str(port) + "/ToCluster", json=json.dumps(args))
         return 200
 
 class DataToDataBase(Resource):
-    def get(self, id):
+    def post(self):
         parser = reqparse.RequestParser()
         for name in dataBaseHeader:
             parser.add_argument(name, type=int, location='json')
         args = parser.parse_args()
-        return 200
+        return args
 
-api.add_resource(OrderFromApp, "/post/ToServerFromApp_<id>")
-api.add_resource(DataToDataBase, "/post/ToDataBase_<id>")
-
-
-app.run(host=host, port=port, debug=True)
+api.add_resource(OrderFromApp, "/post/OrderFromApp_<id>")
+api.add_resource(DataToDataBase, "/post/ToDataBase")
+app.run(host, port, debug=True,threaded=True)
