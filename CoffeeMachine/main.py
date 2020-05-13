d7d91daba84596a0b2cfd3328aae27b238cc27b7
Kerusey <danila.lihh@gmail.com>
2020-05-13 22:55:54 +0300

    some new changes

--- CoffeeMachine/main.py
+++ CoffeeMachine/main.py
@@ -1,58 +1,58 @@
-from Validation import Visualizer
-from CoffeePudding import Barista
-import Request
-import time
+from flask import Flask
+from flask_restful import Resource, Api, reqparse
+import netifaces
+import json
+import requests
 import os
 
-def validateToken(token):
-	if (Request.getToken() != token):
-		return "FAILED"
-	else:
-		return "OK"
+def getLan(): # OK
+	interfaces = netifaces.interfaces()
+	for i in interfaces:
+		if (i == 'lo'):
+			continue
+		iface = netifaces.ifaddresses(i).get(netifaces.AF_INET)
+		if (iface != None):
+			for j in iface:
+				return str(j['addr'])
+
+host = getLan()
+port = 8090
+
+ingredients = ( "coffeeType", "milk", "strength", "sugar", "volume" )
+dataBaseHeader = ( "coffeeID", "water", "sugar", "milk" )
+
+scriptPath = os.path.dirname(os.path.abspath(__file__)) + "/"
+
+MachineSettingsPath = scriptPath + "MachineSettings.json"
+with open(MachineSettingsPath) as json_file:
+	MachineSettings = json.load(json_file)
+
+def postDataToDataBase(coffeeID:int, order:dict):
+	order["coffeeID"] = coffeeID
+	requests.post("http://chaos.fstudio.space:8090/ToDataBase", json=json.dumps(order))
+
+def chooseCoffeeMachine(coffeeMachinePool:dict=coffeeMachinePool): # returns ID of available coffeeMachine from  coffeeMachinePool
+	return 0 # TODO realization of this method
+
+path = os.path.dirname(os.path.abspath(__file__))
+app = Flask(__name__)
+api = Api(app)
+
+class OrderToCluster(Resource):
+	def post(self):
+		parser = reqparse.RequestParser()
+		parser.add_argument("coffeeType", type=str, location='json')
+		for item in ingredients:
+			if (item is "coffeeType"):
+				continue
+			parser.add_argument(item, type=int, location='json')
+		order = parser.parse_args()
+		currentMachine = chooseCoffeeMachine()
+		barista.makeOrder(currentMachine, order)
+		postDataToDataBase(currentMachine, order)
+		return 200
 
 
-def scanActivity():
-	currentToken = Visualizer.visualNewSession()
-	Visualizer.stopSession()
-	Request.postTokenStatus("READYTOSCAN")
 
-	timer = 2
-	while(Request.getTokenStatus() == "READYTOSCAN"):
-		time.sleep(timer)
-		timer += 2
-		if (timer >= 90):
-			timer = 2
-
-	validationStatus = validateToken(currentToken)
-	Request.postTokenStatus(validationStatus)
-	if(validationStatus == "FAILED"):
-		Visualizer.stopSession()
-		return currentToken, True
-
-	return currentToken, False
-
-def finalOrder():
-	Request.postOrderStatus("WAITING")
-
-	timer = 2
-	while(Request.getOrderStatus() == "WAITING"):
-		time.sleep(timer)
-		timer += 2
-		if (timer >= 90):
-			timer = 1      # OK
-
-	return Request.getOrder()
-
-def main():
-	flag = True
-	while flag:
-		generalToken, flag = scanActivity()
-	jsonOrder = finalOrder()
-	print(jsonOrder)
-
-	Request.postBd(jsonOrder, generalToken)
-	# Barista.make(jsonOrder)
-	Visualizer.stopSession()
-
-while(True):
-	main()
+api.add_resource(OrderToCluster, "/post/OrderToCluster")
+app.run(host=host, port=port, debug=True, threaded=True)
