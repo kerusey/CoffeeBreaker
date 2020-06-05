b48ac12425184ca2b327d066b1cf359edb410218
Kerusey <danila.lihh@gmail.com>
2020-06-06 02:58:36 +0300

    Changed 404 page

--- DjangoPyServer/DjangoPyServer/views.py
+++ DjangoPyServer/DjangoPyServer/views.py
@@ -4,3 +4,7 @@ from django.shortcuts import render
 def error_404(request, exception):
 	data = {}
 	return render(request, settings.TEMPLATE_SOURCE_DIR + '404.html', data)
+
+def error_500(request):
+	data = {}
+	return render(request, settings.TEMPLATE_SOURCE_DIR + '500.html', data)
