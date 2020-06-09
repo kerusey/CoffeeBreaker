916a7785f5dd1a81a04788f24301e2d34091b8ee
pavel_em <pavelemelyanov03@mail.ru>
2020-06-10 01:32:55 +0300

    Collect Static files

--- DjangoPyServer/DjangoPyServer/settings.py
+++ DjangoPyServer/DjangoPyServer/settings.py
@@ -87,11 +87,11 @@ DATABASES = {
 }
 
 STATICFILES_DIRS = [
-	os.path.join(BASE_DIR, 'static/'),
 	os.path.join(BASE_DIR, 'templates/'),
 ]
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
