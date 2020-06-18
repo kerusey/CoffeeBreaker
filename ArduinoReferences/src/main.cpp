840bd4bc39e5f6f5157a7a62b7e08e561394a830
kerusey <38439184+kerusey@users.noreply.github.com>
2020-06-18 12:55:43 +0300

    Update main.cpp

--- ArduinoReferences/src/main.cpp
+++ ArduinoReferences/src/main.cpp
@@ -1,6 +1,21 @@
 #include <Arduino.h>
+#include <WiFi.h>
+
+const char* ssid = "CoffeeBreaker";
+const char* password = "CoffeeBreakerTest"; // global constants assigned to your wifi network
+
+void connectToWiFi() {
+	Serial.begin(115200);
+	WiFi.begin(ssid, password);
+	while (WiFi.status() != WL_CONNECTED) {
+		delay(500);
+		Serial.println("Connecting to WiFi..");
+	}
+	Serial.println("Connected to the WiFi network");
+}
 
 void setup() {
+	connectToWiFi();
 	// put your setup code here, to run once:
 }
 
