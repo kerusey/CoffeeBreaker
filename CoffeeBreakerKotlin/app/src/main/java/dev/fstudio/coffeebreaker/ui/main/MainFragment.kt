da6f4475c6c50c25d91d37d7fb5903ca525c3417
syorito-hatsuki <enderdron3@gmail.com>
2021-11-29 07:58:51 +0200

    Init kotlin skelet

---
+++ CoffeeBreakerKotlin/app/src/main/java/dev/fstudio/coffeebreaker/ui/main/MainFragment.kt
@@ -0,0 +1,34 @@
+package dev.fstudio.coffeebreaker.ui.main
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import dev.fstudio.coffeebreaker.databinding.MainFragmentBinding
+
+class MainFragment : Fragment() {
+
+    private lateinit var binding: MainFragmentBinding
+
+    companion object {
+        fun newInstance() = MainFragment()
+    }
+
+    private lateinit var viewModel: MainViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        binding = MainFragmentBinding.inflate(layoutInflater)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel = ViewModelProvider(this)[MainViewModel::class.java]
+        binding.message.text = "Сделан скелет"
+    }
+}
---
+++ CoffeeBreakerKotlin/app/src/main/java/dev/fstudio/coffeebreaker/ui/main/MainViewModel.kt
@@ -0,0 +1,7 @@
+package dev.fstudio.coffeebreaker.ui.main
+
+import androidx.lifecycle.ViewModel
+
+class MainViewModel : ViewModel() {
+
+}
